7863d44630497dd0bae8f0fb631d25af6326b754
sounava2007 <84862434+sounava2007@users.noreply.github.com>
2021-05-29 07:51:51 -0700

    Add files via upload

---
+++ FizzBuzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void fizzbuzz();
+
+int main(){
+    int i;
+    fizzbuzz();
+    return 0;
+}
+
+void fizzbuzz(){
+    int i;
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 15 == 0){
+            printf("FizzBuzz\n");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz\n");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz\n");
+        }
+        else{
+            printf("%d\n", i);
+        }
+    }
+    
+}
